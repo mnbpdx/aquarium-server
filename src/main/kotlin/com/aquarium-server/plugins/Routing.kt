d83bdd005eeff1682769834313e69e9deb105333
Mark Beebe <mark.beebe@kroger.com>
2021-10-02 17:13:14 -0700

    rename package

--- src/main/kotlin/com/aquarium-server/plugins/Routing.kt
+++ src/main/kotlin/com/aquarium-server/plugins/Routing.kt
@@ -1,10 +1,8 @@
-package com.example.plugins
+package com.`aquarium-server`.plugins
 
 import io.ktor.routing.*
-import io.ktor.http.*
 import io.ktor.application.*
 import io.ktor.response.*
-import io.ktor.request.*
 
 fun Application.configureRouting() {
 
